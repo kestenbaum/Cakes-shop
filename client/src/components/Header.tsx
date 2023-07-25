add99077576fa26947a303bc443c06305e6ed5bc
Oleksii Vovneko <kespa71for@gmail.com>
2023-07-25 21:40:47 +0300

    create header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+
+import styles from "@/styles/Header.module.css"
+const Header:FC = () => {
+    return (
+        <header className={styles.header}>
+            Header
+        </header>
+    );
+};
+
+export default Header;
