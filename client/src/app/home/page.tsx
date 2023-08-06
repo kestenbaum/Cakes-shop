e5eaf89a0fef840dd68bdd31b871b49dc3dd3339
Oleksii Vovnenko <kespa71for@gmail.com>
2023-08-06 15:57:28 +0200

    update style background for MainPage

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,22 +1,12 @@
 import React, {FC} from 'react';
 
 import styles from "@/styles/MainPage.module.css"
-import background from "@/assets/main.png"
 
-import Image from "next/image";
 const Page:FC = () => {
     return (
         <div
             className={styles.wrapper}
         >
-            <Image
-                alt={"bg"}
-                src={background}
-                fill={true}
-                style={{
-                    objectFit: "cover"
-                }}
-            />
             <div className="container">
                 <section
                     className={styles.page}
