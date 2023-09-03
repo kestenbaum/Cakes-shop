9a36263732c091854d4b75b1ad0b5ffa2dbaa9d5
Oleksii Vovneko <kespa71for@gmail.com>
2023-09-03 17:00:51 +0300

    --create profile page

---
+++ client/src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+
+import styles from "@/styles/Profile.module.css"
+const Page:FC = () => {
+    return (
+        <section
+            className={styles.wrapper}
+        >
+            Profile
+        </section>
+    );
+};
+
+export default Page;
