507bce76003dd93a4360e4ec14e8dbad4dd33a23
Oleksii Vovneko <kespa71for@gmail.com>
2023-09-04 17:55:20 +0300

    --create profile main block

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,12 +1,37 @@
 import React, {FC} from 'react';
 
 import styles from "@/styles/Profile.module.css"
+import ProfileAsideElement from "@/components/Profile/ProfileAsideElement";
+import {CgProfile} from "react-icons/cg";
 const Page:FC = () => {
+
+    const img = <CgProfile width={24} height={24}/>
+
     return (
         <section
             className={styles.wrapper}
         >
-            Profile
+            <aside
+                className={styles.sidebar}
+            >
+                <ProfileAsideElement
+                    img={img}
+                    link={"Name Vorname"}
+                    href={"/"}
+                />
+                <ProfileAsideElement
+                    img={img}
+                    link={"Name Vorname"}
+                    href={"/"}
+                />
+            </aside>
+            <section className={styles.main}>
+                <h2
+                    className={styles.title}
+                >
+                    Личные данные
+                </h2>
+            </section>
         </section>
     );
 };
