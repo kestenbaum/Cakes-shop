65e8156b5ca4986fd34b6c429d4f1929fc838b0c
kespa <kespa71for@gmail.com>
2023-09-06 23:10:56 +0300

    --create data for getAsideProfilePage and getMenuProfilePage

--- client/src/data/index.ts
+++ client/src/data/index.ts
@@ -1,4 +1,13 @@
-import {FooterMenuProps, IconsProps, NavProps} from "@/interface";
+import {FooterMenuProps, IconsProps, IProfileAsideElement, IProfileMenuElement, NavProps} from "@/interface";
+
+import {CgProfile} from "react-icons/cg";
+import {MdOutlineContactMail} from "react-icons/md";
+import {BsReverseListColumnsReverse, BsTruck} from "react-icons/bs";
+import {AiOutlineHeart} from "react-icons/ai";
+import {TbPigMoney} from "react-icons/tb";
+import {RiMoneyDollarCircleLine} from "react-icons/ri";
+import {IoMdLogIn} from "react-icons/io";
+import {GrCircleInformation} from "react-icons/gr";
 
 export const getDataIcons: IconsProps[] = [
     {
@@ -113,5 +122,71 @@ export const getFooterMenu:FooterMenuProps[] = [
                 href: "/delivery"
             },
         ],
+    }
+]
+
+export const getAsideProfilePage:IProfileAsideElement[] = [
+    {
+        id: 1,
+        img: CgProfile,
+        href: "/",
+        link: "User Surname"
+    },
+    {
+        id: 2,
+        img: BsReverseListColumnsReverse,
+        href: "/",
+        link: "Мои заказы"
+    },
+    {
+        id: 3,
+        img: AiOutlineHeart,
+        href: "/",
+        link: "Список желаний"
+    },
+    {
+        id: 4,
+        img: TbPigMoney,
+        href: "/",
+        link: "Мой кошелек"
+    },
+    {
+        id: 5,
+        img: RiMoneyDollarCircleLine,
+        href: "/",
+        link: "Мой бонусный счет"
+    },
+]
+
+export const getMenuProfilePage:IProfileMenuElement[] = [
+    {
+        id: 1,
+        img: CgProfile,
+        title: 'Личные данные',
+        content: "Личные данные"
+    },
+    {
+        id: 1,
+        img: MdOutlineContactMail,
+        title: 'Контакты',
+        content: "Контакты"
+    },
+    {
+        id: 1,
+        img: BsTruck,
+        title: 'Адрес доставки',
+        content: "Адрес доставки"
+    },
+    {
+        id: 1,
+        img: IoMdLogIn,
+        title: 'Логин',
+        content: "Логин"
+    },
+    {
+        id: 1,
+        img: GrCircleInformation,
+        title: 'Дополнительная информация',
+        content: "Дополнительная информация"
     }
 ]
