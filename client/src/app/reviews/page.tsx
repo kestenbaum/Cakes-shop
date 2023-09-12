f8eb6610cb6a612e80f349c98db294444ee0a28a
Oleksii Vovnenko <kespa71for@gmail.com>
2023-09-12 22:17:53 +0300

    create (reviews page)

---
+++ client/src/app/reviews/page.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from 'react';
+
+import styles from "@/app/reviews/reviews.module.css"
+
+const Page:FC = () => {
+    return (
+        <section
+            className={styles.wrapper}
+        >
+            reviews
+        </section>
+    );
+};
+
+export default Page;
