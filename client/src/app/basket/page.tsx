fd854eafc9d8bf61b86cb42f3c2e48be96116d3a
Oleksii Vovnenko <kespa71for@gmail.com>
2023-09-18 19:50:51 +0200

    create (page) create page basket

---
+++ client/src/app/basket/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from "@/app/basket/basket.module.css"
+const Page = () => {
+    return (
+        <section className={styles.wrapper}>
+            Basket
+        </section>
+    );
+};
+
+export default Page;
