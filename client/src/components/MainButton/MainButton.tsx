06b0b5003c908f61a34624cbe1e284e50ad3ca80
Oleksii Vovnenko <kespa71for@gmail.com>
2023-09-23 16:04:56 +0200

    fix (custom btn) fix props

--- client/src/components/MainButton/MainButton.tsx
+++ client/src/components/MainButton/MainButton.tsx
@@ -4,10 +4,13 @@ import styles from "@/components/MainButton/MainButton.module.css"
 import {IButton} from "@/interface";
 
 const MainButton:FC<IButton> = (props) => {
-    const  {children} = props
+    const  {children, ...rest} = props
 
     return (
-        <button className={styles.btn}>
+        <button
+            className={styles.btn}
+            {...rest}
+        >
             <span
                 className={styles.text__btn}
             >
