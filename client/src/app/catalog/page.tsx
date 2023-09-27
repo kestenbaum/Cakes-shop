7b4fdad69a8da720234294f47f960347897a22a8
Oleksii Vovnenko <kespa71for@gmail.com>
2023-09-27 16:22:55 +0200

    create (tabs) create tabs for catalog

--- client/src/app/catalog/page.tsx
+++ client/src/app/catalog/page.tsx
@@ -5,7 +5,9 @@ import PathLink from "@/components/PathLink/PathLink";
 import styles from "@/app/catalog/catalog.module.css"
 import CatalogNavigation from "@/components/CatalogNavigation/CatalogNavigation";
 
-import {getTitleTest} from "@/data";
+import {getMainProductTest, getTitleTest} from "@/data";
+import MainButton from "@/components/UI/MainButton/MainButton";
+
 
 const Page:FC = () => {
     const [path, setPath] = useState<string>("Торты")
@@ -17,20 +19,44 @@ const Page:FC = () => {
                 path={path}
                 setPath={setPath}
             />
-            <div className={styles.main__block}>
+            <div className={styles.block}>
                 {
                     getTitleTest.length === 0 ?
                     <p>
                         Здесь пока нечего нет...
                     </p>
                     :
-                        getTitleTest.map((title) => {
-                          return <div>
-                                <h2
-                                    onClick={() => setPath(title.title)}
-                                >{title.title}</h2>
-                          </div>
-                      })
+                    <div
+                        className={styles.content}
+                    >
+                        <div
+                            className={styles.tabs}
+                        >
+                            {getTitleTest.map(tab => {
+                                return <div
+                                    className={styles.tab}
+                                    onClick={() => setPath(tab.title)}
+                                >
+                                    {tab.title}
+                                </div>
+                            })}
+                        </div>
+                        <div
+                            className={styles.catalog}
+                        >
+                            {getMainProductTest.map(element=> {
+                                return <div>
+                                    {element.title}
+                                    {element.old__price}
+                                    {element.new__price}
+                                    <div>
+                                        {element.category}
+                                    </div>
+                                    <MainButton>В корзину</MainButton>
+                                </div>
+                            })}
+                            </div>
+                    </div>
                 }
             </div>
         </section>
