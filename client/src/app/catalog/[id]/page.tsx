2f9311ae00c2cc3c0e36407f9eef20396492a89e
Oleksii Vovnenko <kespa71for@gmail.com>
2023-10-03 11:20:54 +0300

    create (page) create page for product

--- client/src/app/catalog/[id]/page.tsx
+++ client/src/app/catalog/[id]/page.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
 
+import img from "@/assets/Frame 76.png"
+
+import MainButton from "@/components/UI/MainButton/MainButton";
 import {getMainProductTest} from "@/data";
 import {IPage} from "@/interface";
 
 import "./style.css"
+import Image from "next/image";
+
 
 const Page = ({params: {id}}: IPage) => {
-
     const element
         = getMainProductTest.find(product => Number(id) === product.category)
 
-    console.log(element)
-    console.log("aa")
     return (
-        <div className={"wrapper"}>
-            <p>Post page{id}</p>
-            <div>Title {element?.title}</div>
-            <div>Price {element?.old__price}</div>
-        </div>
+        <section className={"wrapper"}>
+            <div className="content">
+                <div className={"block"}>
+                    <div className={"info"}>
+                        <h2 className={"title"}>{element?.title}</h2>
+                        <p>{element?.description}</p>
+                        <span>Страна производитель: {element?.country}</span>
+                        <div className={"price"}>
+                            <span>Старая цена {element?.old__price} Гривен</span>
+                            <span>Новая цена {element?.new__price} Гривен</span>
+                        </div>
+                        <span>Вес: {element?.weight} гр</span>
+                        <div>
+                            <MainButton>В корзину</MainButton>
+                        </div>
+                    </div>
+                    <div>
+                        <Image
+                            src={img}
+                            alt={"image for element"}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
