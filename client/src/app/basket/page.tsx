aaee17f2cf9afc1be80af372a2729340707a2aba
Oleksii Vovnenko <kespa71for@gmail.com>
2023-10-03 11:28:05 +0300

    update (Basket page) add block for product

--- client/src/app/basket/page.tsx
+++ client/src/app/basket/page.tsx
@@ -8,13 +8,16 @@ const Page: FC = () => {
     return (
         <section className={styles.wrapper}>
             <h1 className={styles.title}>Корзина</h1>
-            <MainButton>Очистить корзину</MainButton>
+            <div>
+                <MainButton>Очистить корзину</MainButton>
+            </div>
             <section className={styles.section__basket}>
                 <div className={styles.product}>
-                    <div>1</div>
-                    <div>1</div>
-                    <div>1</div>
-                    <div>1</div>
+                    {true ?
+                        <span>Товаров пока нет</span>
+                        :
+                        null
+                    }
                 </div>
                 <AsideBasket/>
             </section>
