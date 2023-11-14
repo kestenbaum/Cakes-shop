b6315fc7f8b9e1dbc6ff71e5e80d7cd6801a3966
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-15 00:20:27 +0300

    fix styled for Accordion

--- client/src/components/UI/Accordion/Accordion.tsx
+++ client/src/components/UI/Accordion/Accordion.tsx
@@ -1,44 +1,85 @@
 "use client"
 import React, {useState} from 'react';
 
-import styles from "@/components/UI/Accordion/Accordion.module.css"
+import styled from "styled-components";
 import {FaArrowAltCircleDown, FaArrowAltCircleUp} from "react-icons/fa";
 const Accordion = (props:any) => {
     const [isShowing, setIsShowing] = useState(false);
-
-    const toggle = () => {
-        setIsShowing(!isShowing);
-    };
+    const toggle = () => setIsShowing(!isShowing);
 
     return (
-        <div
-            className={styles.wrapeper}
-        >
-            <button
-                className={styles.btn}
+        <Wrapper>
+            <Button
                 onClick={toggle}
                 type="button"
             >
-                <p className={styles.wrapper__title}>
-                   {props.img}
-                    <span className={styles.title}>{props.title}</span>
-                </p>
-                {
-                    isShowing ?
-                        <FaArrowAltCircleUp className={styles.icon}/>
-                        :
-                        <FaArrowAltCircleDown className={styles.icon}/>
-                }
-            </button>
-            <div
-                className={styles.content}
-                style={{ display: isShowing ? "block" : "none" }}
+                <Title>
+                    {props.img}
+                    <span>{props.title}</span>
+                </Title>
+                {isShowing ? <ArrowUp/> : <ArrowDown/>}
+            </Button>
+            <Content
+                $props={isShowing}
                 dangerouslySetInnerHTML={{
                     __html: props.content
                 }}
             />
-        </div>
+        </Wrapper>
     );
 };
 
 export default Accordion;
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin-bottom: 15px;
+  line-height: 15px;
+  border: 1px solid rgba(209, 213, 219, 0.5);
+`
+
+const Button = styled.button`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  position: relative;
+  text-align: left;
+  background: transparent;
+  outline: none;
+  cursor: pointer;
+  padding: 24px;
+  transition: all .2s ease-in-out;
+  border: 3px solid #e9e9e9;
+  border-radius: 4px;
+  margin-bottom: 10px ;
+  :hover{
+    background-color: #CA8B81;
+  }
+`
+
+const Title = styled.p`
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  gap: 10px;
+`
+
+const ArrowUp = styled(FaArrowAltCircleUp)`
+  width: 24px;
+  height: 24px;
+`
+
+const ArrowDown = styled(FaArrowAltCircleDown)`
+  width: 24px;
+  height: 24px;
+`
+
+const Content = styled.div<{$props?: boolean}>`
+  display: ${props => props.$props ? "block" : "none"};
+  width: 100%;
+  position: relative;
+  padding: 24px;
+  transition: all .2s ease-in-out;
+  border: 3px solid #e9e9e9;
+  border-radius: 4px;
+`
