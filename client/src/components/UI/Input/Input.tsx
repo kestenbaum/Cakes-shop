f03e0e4ecdb535966c25cb1517c8257a111131b3
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-15 00:22:47 +0300

    fix styled for Input custom

--- client/src/components/UI/Input/Input.tsx
+++ client/src/components/UI/Input/Input.tsx
@@ -1,18 +1,32 @@
 import React, {FC} from 'react';
 
-import styles from "@/components/UI/Input/Input.module.css"
+import styled from "styled-components";
 import {IInput} from "@/interface";
 
 const Input:FC<IInput> = ({name, label, ...rest}) => {
     return (
-        <div>
+        <>
             <label htmlFor={name}>{label}</label>
-            <input
-                className={styles.input}
-                {...rest}
-            />
-        </div>
+            <InputStyle {...rest}/>
+        </>
     );
 };
 
 export default Input;
+
+const InputStyle = styled.input`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  min-width: 200px;
+  width: 100%;
+  height: 40px;
+  background: #FFF;
+  padding: 10px;
+  border: 1px solid #FF5151;
+  transition: all .5s;
+  :focus{
+    outline: none;
+    border-radius: 15px;
+  }
+`
