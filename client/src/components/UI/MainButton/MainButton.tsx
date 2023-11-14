2b7e8a4f99e9c39f5869968930f64f075573474d
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-15 00:25:36 +0300

    fix styled for Button custom

--- client/src/components/UI/MainButton/MainButton.tsx
+++ client/src/components/UI/MainButton/MainButton.tsx
@@ -1,23 +1,38 @@
 import React, {FC} from 'react';
 
-import styles from "@/components/UI/MainButton/MainButton.module.css"
+import styled from "styled-components";
 import {IButton} from "@/interface";
 
 const MainButton:FC<IButton> = (props) => {
     const  {children, ...rest} = props
 
     return (
-        <button
-            className={styles.btn}
-            {...rest}
-        >
-            <span
-                className={styles.text__btn}
-            >
-                {children}
-           </span>
-        </button>
+        <ButtonStyle {...rest} >
+            <Content>{children}</Content>
+        </ButtonStyle>
     );
 };
 
 export default MainButton;
+
+const ButtonStyle = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid #FF5151;
+  cursor: pointer;
+  transition: 0.3s;
+`
+
+const Content = styled.span`
+  font-family: Happy Monkey, sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #FF5151;
+  :hover{
+    transform: scale(1.05);
+  }
+`
