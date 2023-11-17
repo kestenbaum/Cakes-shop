a7a30026b79c015b3d14c26d451970021fa7c43a
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-17 21:40:08 +0300

    update styled for Basket page

--- client/src/app/basket/page.tsx
+++ client/src/app/basket/page.tsx
@@ -1,28 +1,64 @@
+"use client"
 import React, {FC} from 'react';
 
-import styles from "@/app/basket/basket.module.css"
+import styled from "styled-components";
 import MainButton from "@/components/UI/MainButton/MainButton";
 import AsideBasket from "@/components/Aside/AsideBasket";
 
 const Page: FC = () => {
     return (
-        <section className={styles.wrapper}>
-            <h1 className={styles.title}>Корзина</h1>
+        <Wrapper>
+            <Title>Корзина</Title>
             <div>
                 <MainButton>Очистить корзину</MainButton>
             </div>
-            <section className={styles.section__basket}>
-                <div className={styles.product}>
+            <Basket>
+                <Product>
                     {true ?
                         <span>Товаров пока нет</span>
                         :
                         null
                     }
-                </div>
+                </Product>
                 <AsideBasket/>
-            </section>
-        </section>
+            </Basket>
+        </Wrapper>
     );
 };
 
 export default Page;
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background: #FFEBF0;
+`
+
+const Title = styled.h1`
+  font-family: Prata, sans-serif;
+  font-size: 50px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-bottom: 10px;
+  margin-top: 25px;
+`
+
+const Basket = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 25px;
+  width: 100%;
+  gap: 50px;
+`
+
+const Product = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+  background: #fff;
+  border-radius: 15px;
+  padding: 22px 10px;
+  flex: 70%;
+`
