6c4b8850f2a05e0f94725e963ccd801d56b49e43
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-17 22:16:10 +0300

    fix styled page profile

--- client/src/app/reviews/page.tsx
+++ client/src/app/reviews/page.tsx
@@ -1,15 +1,26 @@
+"use client"
 import React, {FC} from 'react';
 
-import styles from "@/app/reviews/reviews.module.css"
+import styled from "styled-components"
 
 const Page:FC = () => {
     return (
-        <section
-            className={styles.wrapper}
-        >
+        <Wrapper>
             reviews
-        </section>
+        </Wrapper>
     );
 };
 
 export default Page;
+
+const Wrapper = styled.section`
+  display: flex;
+  gap: 20px;
+  background-color: #FFEBF0;
+  width: 100%;
+  max-width:1198px;
+  padding: 104px 15px 0;
+  margin: 0 auto;
+  height: 100%;
+  min-height: calc(100vh - 104px);
+`
--- client/src/components/Profile/ProfileAsideElement.tsx
+++ client/src/components/Profile/ProfileAsideElement.tsx
@@ -1,19 +1,14 @@
 import React, {FC} from 'react';
 
-import styles from "@/app/profile/profile.module.css"
 import {IProfileAsideElement} from "@/interface";
 
-import Image from "next/image";
 import Link from "next/link";
 
 const ProfileAsideElement:FC<IProfileAsideElement> = ({img, link, href}) => {
     return (
-        <Link
-            className={styles.profile__element}
-            href={href}
-        >
+        <Link href={href}>
             {img}
-            <div className={styles.description}>
+            <div>
                 {link}
             </div>
         </Link>
