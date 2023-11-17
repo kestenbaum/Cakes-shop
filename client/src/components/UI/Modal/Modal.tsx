e000b096e990c1b14d88403797d539022eeff78d
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-17 21:32:09 +0300

    update styled for custom Modal

--- client/src/components/UI/Modal/Modal.tsx
+++ client/src/components/UI/Modal/Modal.tsx
@@ -1,22 +1,55 @@
+"use client"
 import React, {FC} from 'react';
-
 import {IModal} from "@/interface";
-import "./modal.css"
+import styled from "styled-components"
+
 
 const Modal:FC<IModal> = ({children, showModal, setShowModal}) => {
     return (
-        <section
-            className={showModal ? "modal active" : "modal"}
+        <Wrapper
+            $props = {showModal}
             onClick={() => setShowModal(false)}
         >
-            <div
-                className={showModal ? "wrapper active" : "wrapper"}
+            <Content
                 onClick={e => e.stopPropagation()}
+                $props = {showModal}
             >
                 {children}
-            </div>
-        </section>
+            </Content>
+        </Wrapper>
     );
 };
 
 export default Modal;
+
+const Wrapper = styled.section<{$props?:boolean}>`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  opacity: ${props => props.$props ? 1 : 0};
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 9999;
+  pointer-events: ${props => props.$props ? "all" : "none"};
+  transform: scale(2);
+  transition: .4s all;
+`
+
+const Content = styled.div<{$props?:boolean}>`
+  display: flex;
+  opacity: ${props => props.$props ? 1 : 0};
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 200px;
+  padding: 25px;
+  border-radius: 15px;
+  background-color: #ffffff;
+  color: #1a1a1a;
+  transform: scale(1);
+  transition: .4s all;
+`
