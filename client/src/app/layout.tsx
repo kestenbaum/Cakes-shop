225f4290e734cd768c466819085f297db43b0bd7
Oleksii Vovnenko <kespa71for@gmail.com>
2023-11-18 04:44:41 +0300

    update styled main page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>
             {children}
-        </main>
       </body>
     </html>
   )
